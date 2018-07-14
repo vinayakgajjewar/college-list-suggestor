605b7125b16499da157f65fd5a8e9e4a2a87bb1c
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-07-14 10:23:46 -0700

    Remove redundant 'next' parameter

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.sendFile(__dirname.split('/routes')[0] + "/views/about.html");
 });
 
